b809e2de7ea1ed4d5fa0f1fb4d471612650d5f97
lazar <dasidasi86@gmail.com>
2026-03-03 08:40:05 +0200

    deploy project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.svg"
+  import "../lib/styles/global.css"
 
-	let { children } = $props();
+  let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
+<<<<<<< Updated upstream
 {@render children()}
+=======
+<div class="main">
+  {@render children()}
+</div>
+
+<style>
+  .main {
+    padding: 1rem;
+  }
+</style>
+>>>>>>> Stashed changes
