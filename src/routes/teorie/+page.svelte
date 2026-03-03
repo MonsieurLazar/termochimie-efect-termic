55d5aec19452e996f8882ca95cea4ec3e9dee9df
Cristi <176015294+CrisBricolone@users.noreply.github.com>
2026-03-03 15:45:11 +0200

    Introductory section + slight formatting

--- src/routes/teorie/+page.svelte
+++ src/routes/teorie/+page.svelte
@@ -1,1 +1,55 @@
+<script lang='ts'>
+
+</script>
+
+<div id = "teorie">
 <h1>Teorie</h1>
+</div>
+
+<!-- Paragraf introductiv -->
+<p class = "indent_paragraph">
+    Căldura de reacţie este efectul termic ce însoţeşte o reacţie chimică şi reprezintă
+cantitatea de căldura absorbită sau degajată în timpul unei reacţii chimice. Ea depinde numai de
+starea iniţială şi starea finală a sistemului chimic, nu şi de drumul parcurs de sistem în timpul
+evoluţiei sale.
+Căldura de reacţie se determina în majoritatea cazurilor în condiţii de presiune constantă,
+Q<sub>p</sub> şi mai rar în condiţii de volum constant, Q<sub>v</sub>, la o anumită temperatură. 
+Căldurile de reacţie la presiune constantă Q<sub>p</sub> = ∆H sunt chiar entalpiile de reacţie. Procesele
+chimice în care sistemul reactant cedează căldura mediului exterior se numesc <span class = "highlight">
+reacţii exoterme ∆H &lt O </span>, iar cel în care sistemul absoarbe căldură din mediul exterior 
+<span class = "highlight"> ∆H &gt O se numesc reacţii endoterme </span>.
+</p>
+
+<!-- Descriere lucrarii -->
+<h1><strong>1. Determinarea entalpiei de utilizare</strong></h1>
+<h2><strong> Principiul lucrarii</strong></h2>
+<p class = "indent_paragraph">
+Căldura de neutralizare a unui acid cu o bază reprezintă cantitatea de căldură degajată la
+formarea unui mol de apă (H<sub>2</sub>O) dintr-un ion gram de protoni hidrataţi H<sub>3</sub>O<sup>+</sup>
+şi un ion gram de grupe hidroxil HO<sup>−</sup>. Pentru reacţiile dintre acizii şi bazele tari, 
+în soluţii apoase diluate, entalpia de neutralizare are aceeaşi valoare, egală cu -13,7 kcal/mol, 
+sau – 57,3kJ/mol la 18<sup>o</sup>C (291 K) şi 1 atm, indiferent de natura acidului şi a bazei.
+</p>
+
+<style>
+    /* Paragraf cu indentare */
+    .indent_paragraph{
+        font-size: 1.3rem;
+        text-indent: 2rem;
+    }
+
+    /* Formatare titlu mare  */
+    #teorie{
+        display: flex;
+        justify-content: center;
+        font-size: 1.7rem;
+    }
+
+    /*Pentru informatii importante*/
+    .highlight{
+        color: yellow;
+        background-color: lightgreen;
+        font-weight: bold;
+    }
+
+</style>
