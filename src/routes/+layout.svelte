ac6e2d1aa5f81aed818e641c05be457d97f88409
Cristi <176015294+CrisBricolone@users.noreply.github.com>
2026-05-06 20:35:42 +0300

    add: background image and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,4 +12,31 @@
   .main {
     padding: 1rem;
   }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    position: relative; 
+    background-color: #ffffff; 
+  }
+
+  :global(body::before) {
+    content:"";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    background-image: url('/design/background/background_1920_1200.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    image-rendering: pixelated;
+
+    opacity: 20%;
+
+    pointer-events: none;
+  }
 </style>
