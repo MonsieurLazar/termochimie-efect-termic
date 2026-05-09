5934a9b5dec5613f16ac79195d525b8df1d718e9
Cristi <176015294+CrisBricolone@users.noreply.github.com>
2026-05-09 15:21:39 +0300

    add: butelcuta plina cu acid

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -255,7 +255,7 @@ const createGlass = (
 
 export const engine = new Engine([
   createInfiniteSource("NaOH sol. 10%", { NaOH_aq: 0.3, H2O: 0.7 }, 2, 4, "#ffbdbd", "/design/300x300/subst_inf_300.png"),
-  createInfiniteSource("HCl sol. 10%", { HCl_aq: 0.3, H2O: 0.7 }, 2, 32, "#b6ffd0","/design/300x300/subst_inf_300.png"),
+  createInfiniteSource("HCl sol. 10%", { HCl_aq: 0.3, H2O: 0.7 }, 2, 32, "#b6ffd0","/design/300x300/subst_inf_302_440.png"),
   createInfiniteSource("Distilled H2O", { H2O: 1 }, 2, 60, "#7accff","/design/300x300/apa_distilata_300.png"),
   createGlass("Main Glass", 30, 52, 150, "/design/300x300/calorimetru_300.png", "", 15, false),
   createGlass("Secondary Glass", 50, 55, 100, "/design/300x300/erlenmeyer_300.png", "", 8),
